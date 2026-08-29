eaac6d846140fead351eb58cc15f89ba189edf4a
Ubuntu <ubuntu@ip-172-31-47-1.sa-east-1.compute.internal>
2026-08-29 21:33:54 +0000

    feat: add cloudflare

--- core/settings.py
+++ core/settings.py
@@ -120,5 +120,14 @@ MEDIA_ROOT = BASE_DIR / 'media'
 
 REST_FRAMEWORK = {
     'DEFAULT_FILTER_BACKENDS': ['rest_framework.filters.OrderingFilter'],
+    'DEFAULT_THROTTLE_CLASSES': [
+        'rest_framework.throttling.AnonRateThrottle',
+        'rest_framework.throttling.UserRateThrottle'
+    ],
+    'DEFAULT_THROTTLE_RATES': {
+        'anon': '5/minute',
+        'user': '10/minute'
+    }
 }
-CONN_MAX_AGE = 60
+
+TURNSTILE_SECRET_KEY = config('TURNSTILE_SECRET_KEY', default='')
